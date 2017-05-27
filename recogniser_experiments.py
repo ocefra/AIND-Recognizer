89894788024e65f84cb875fc89fe588863d9b09c
gizmo <edieabibula@gmail.com>
2017-05-27 17:39:27 +0100

    Implement workflow for experimenting with recogniser

--- recogniser_experiments.py
+++ recogniser_experiments.py
@@ -1,54 +1,84 @@
 #! /home/gizmo/anaconda3/envs/aind/bin/python
-import warnings
+
 import numpy as np
-from my_model_selector import SelectorBIC, SelectorDIC, SelectorCV
-from my_recognizer import recognize
-from asl_utils import show_errors
+# import pandas as pd
+import pickle
 from asl_data import AslDb
-from hmmlearn.hmm import GaussianHMM
 
-asl = AslDb() # initializes the database
+import warnings
+from hmmlearn import GaussianHMM
+from my_model_selectors import SelectorCV, SelectorBIC, SelectorDIC
+from my_recognizer import recognize
+# from asl_utils import show_errors
+from asl_utils import write_errors_to_file
 
-# Make 'ground' features
-asl.df['grnd-ry'] = asl.df['right-y'] - asl.df['nose-y']
-asl.df['grnd-rx'] = asl.df['right-x'] - asl.df['nose-x']
-asl.df['grnd-ly'] = asl.df['left-y'] - asl.df['nose-y']
-asl.df['grnd-lx'] = asl.df['left-x'] - asl.df['nose-x']
+########################################
+######## DATA PREPARATION ##############
+########################################
 
-# collect the features into a list
-features_ground = ['grnd-rx','grnd-ry','grnd-lx','grnd-ly']
+# Initialise the database
+print("Initialising database...")
+asl = AslDb() 
 
 
-# Normalised features
-features_norm = ['norm-rx', 'norm-ry', 'norm-lx','norm-ly']
-features_to_norm = ['right-x', 'right-y', 'left-x', 'left-y']
+# Make additional features
 
+# N.B. The feature-making functions below may not be completely uniform because
+# they were done as piecewise exercises, and only later collected here and
+# adapted.
+
+
+########################################
+## 'GROUND' FEATURES
+
+def make_ground_feature(side_initial, coord):
+    side = 'right' if side_initial == 'r' else 'left'
+    col_name = side + '-' + coord
+    nose_col_name = 'nose-' + coord
+    ground_col_name = 'grnd-' + side_initial + coord
+    asl.df[ground_col_name] = asl.df[col_name] - asl.df[nose_col_name]
+
+print("Making ground features...")
+for side_initial in ['r', 'l']:
+    for coord in ['x', 'y']:
+        make_ground_feature(side_initial, coord)
+
+# Collect the ground feature names into a list
+features_ground = [f for f in list(asl.df) if f.startswith('grnd')]
+
+
+########################################
+## NORMALISED FEATURES
+
+features_to_norm = [side + '-' + coord for side in ['right', 'left'] \
+                                       for coord in ['x', 'y']]
+features_norm = ['norm-' + side_initial + coord for side_initial in ['r', 'l'] \
+                                       for coord in ['x', 'y']]
+
+# Make dfs with means and std for all variables
 df_means = asl.df.groupby('speaker').mean()
-asl.df['left-x-mean']= asl.df['speaker'].map(df_means['left-x'])
 df_std = asl.df.groupby('speaker').std()
 
 def make_aux_cols(feature_name):
-    """Add columns for mean and std to database."""
+    """Add columns for mean and std of feature_name to database."""
     mean_col_name = feature_name + "-mean"
     std_col_name = feature_name + "-std"
-    asl.df[mean_col_name] =         asl.df['speaker'].map(df_means[feature_name])
+    asl.df[mean_col_name] = asl.df['speaker'].map(df_means[feature_name])
     asl.df[std_col_name] = asl.df['speaker'].map(df_std[feature_name])
 
-for feature in features_to_norm:
-    make_aux_cols(feature)
-
-def normalise_feature(feature_name, normalised_feature_name):
+def normalise_feature(feature_name, norm_feature_name):
     mean_col_name = feature_name + "-mean"
     std_col_name = feature_name + "-std"
-    asl.df[normalised_feature_name] = (asl.df[feature_name] - asl.df[mean_col_name]) / asl.df[std_col_name]
+    asl.df[norm_feature_name] = (asl.df[feature_name] - asl.df[mean_col_name]) / asl.df[std_col_name]
 
+print("Making normalised features...")
 for i in range(len(features_to_norm)):
+    make_aux_cols(features_to_norm[i])
     normalise_feature(features_to_norm[i], features_norm[i])
 
 
-# #### Polar coordinates
-
-features_polar = ['polar-rr', 'polar-rtheta', 'polar-lr', 'polar-ltheta']
+########################################
+## POLAR COORDINATES
 
 def make_polar_coords(side):
     side_initial = side[0]
@@ -57,42 +87,38 @@ def make_polar_coords(side):
     x_col_name = 'grnd-' + side_initial + 'x'
     y_col_name = 'grnd-' + side_initial + 'y'
     asl.df[r_col_name] = np.sqrt(np.square(asl.df[x_col_name]) + np.square(asl.df[y_col_name]))
-    # Swap x and y (cf. instructions)
+    # Swap x and y (cf. project instructions)
     asl.df[r_theta_name] = np.arctan2(asl.df[x_col_name], asl.df[y_col_name])
 
+print("Making polar coordinate features...")
 for side in ['left', 'right']:
     make_polar_coords(side)
 
+features_polar = [f for f in list(asl.df) if f.startswith('polar')]
 
-# #### Delta features
 
-features_delta = ['delta-rx', 'delta-ry', 'delta-lx', 'delta-ly']
+########################################
+## DELTA FEATURES
 
 def make_delta(side, coord):
     side_initial = side[0]
     col_name = side + '-' + coord
-    #col_name = 'norm' + '-' + side_initial + coord # on normalised version
     delta_col_name = 'delta-' + side_initial + coord
     asl.df[delta_col_name] = asl.df[col_name].diff().fillna(value=0)
 
+print("Making delta features...")
 for side in ['left', 'right']:
     for coord in ['x', 'y']:
         make_delta(side, coord)
 
+features_delta = ['delta-' + side_initial + coord for side_initial in ['r', 'l'] \
+                                                  for coord in ['x', 'y']]
 
-# #### Custom features
 
-features_custom_1 = ['rx-lx', 'ry-ly']
-features_custom_2 = ['scaled-rx-lx', 'scaled-ry-ly']
-features_custom_3 = ['delta-rx-lx', 'delta-ry-ly',
-                     'delta-nose-x', 'delta-nose-y']
-features_custom_4 = ['scaled-delta-rx-lx', 'scaled-delta-ry-ly',
-                     'scaled-delta-nose-x', 'scaled-delta-nose-y']
-features_custom_5 = ['scaled-delta-rx', 'scaled-delta-ry',
-                     'scaled-delta-lx', 'scaled-delta-ly']
-features_custom_6 = ['norm-polar-rr', 'norm-polar-rtheta',
-                     'norm-polar-lr', 'norm-polar-ltheta']
+########################################
+## CUSTOM FEATURES
 
+### Difference between coordinates of right and left hand in same frame
 def make_hands_diff(coord):
     """Make column for difference between right and left 'coord' (x or y)."""
     diff_col_name = 'r' + coord + '-l' + coord
@@ -100,16 +126,28 @@ def make_hands_diff(coord):
     left_col_name = 'left-' + coord
     asl.df[diff_col_name] = asl.df[right_col_name] - asl.df[left_col_name]
 
+print("Making custom_1 features...")
 for coord in ['x', 'y']:
     make_hands_diff(coord)
 
+features_custom_1 = ['r' + coord + '-' + 'l' + coord for coord in ['x', 'y']]
+
+
+### Delta between nose coordinates and between difference in hands coordinates, between consecutive frames
 def make_delta(feature):
     delta_col_name = 'delta-' + feature
     asl.df[delta_col_name] = asl.df[feature].diff().fillna(value=0)
 
-for feat in ['rx-lx', 'ry-ly', 'nose-x', 'nose-y']:
+feat_for_delta = ['nose-' + coord for coord in ['x', 'y']] + features_custom_1
+
+print("Making custom_2 features...")
+for feat in feat_for_delta:
     make_delta(feat)
 
+features_custom_2 = ['delta-' + f for f in feat_for_delta]
+
+
+### Scaled version of original delta features 
 def scale_feature(feature_name):
     scaled_feature_name = 'scaled-' + feature_name
     feat = asl.df[feature_name]
@@ -117,74 +155,88 @@ def scale_feature(feature_name):
     feat_max = feat.max()
     asl.df[scaled_feature_name] = (feat - feat_min) / (feat_max - feat_min)
 
-all_features = asl.df.columns.values.tolist()
-delta_features = [feat for feat in all_features if feat.startswith('delta')]
+print("Making custom_3 features...")
+for feat in features_delta:
+    scale_feature(feat)
 
-for delta_feature in delta_features:
-    scale_feature(delta_feature)
+features_custom_3 = ['scaled-' + f for f in features_delta]
 
-for feature in ['rx-lx', 'ry-ly']:
-    scale_feature(feature)
 
-# Recompute means and std dfs to include the new features.
+### Scaled version of difference between right and left hand coordinates, in same frame
+print("Making custom_4 features...")
+for feat in features_custom_1:
+    scale_feature(feat)
+
+features_custom_4 = ['scaled-' + f for f in features_custom_1]
+
+
+### Scaled version of delta between nose coordinates and between difference in hands
+# coordinates, between consecutive frames
+print("Making custom_5 features...")
+for feat in features_custom_2:
+    scale_feature(feat)
+
+features_custom_5 = ['scaled-' + f for f in features_custom_2]
+
+
+### Normalised polar coordinates
+# Recompute means and std dfs to include the new features
 df_means = asl.df.groupby('speaker').mean()
 df_std = asl.df.groupby('speaker').std()
 
-features_to_norm = [f for f in list(asl.df) if f.startswith('polar') \
-                    and not f.endswith('mean') and not f.endswith('std')]
-# features_norm is taken and must not be reassigned because it is used in the test
-features_norm_2 = ['norm-' + f for f in features_to_norm]
+features_custom_6 = ['norm-' + f for f in features_polar]
 
-def make_aux_cols(feature_name):
-    mean_col_name = feature_name + "-mean"
-    std_col_name = feature_name + "-std"
-    asl.df[mean_col_name] = asl.df['speaker'].map(df_means[feature_name])
-    asl.df[std_col_name] = asl.df['speaker'].map(df_std[feature_name])
-
-for feature in features_to_norm:
-    make_aux_cols(feature)
-
-for i in range(len(features_to_norm)):
-    normalise_feature(features_to_norm[i], features_norm_2[i])
+print("Making custom_6 features...")
+for i in range(len(features_polar)):
+    make_aux_cols(features_polar[i])
+    normalise_feature(features_polar[i], features_custom_6[i])
 
 
+#######################################
+# SAVE THE PROCESSED DATA
+# Collect all feature name sets
+suffixes = ['ground', 'delta', 'norm', 'polar']
+all_feature_set_names = ['features_' + suffix for suffix in suffixes]
+custom_feature_set_names = ['features_custom_' + str(i) for i in range(1, 7)]
+all_feature_set_names += custom_feature_set_names
+all_features = {f_set_name: eval(f_set_name) for f_set_name in all_feature_set_names}
+
+
+# Save the data
+print("Saving data...")
+pickle.dump(asl, open('asl.pickle', 'wb'))
+pickle.dump(all_features, open('features.pickle', 'wb'))
 
 
 
-
-# ### Recognizer
-# ##### Train the full training set
+########################################
+###### RECOGNISER EXPERIMENTS ##########
+########################################
 
 def train_all_words(features, model_selector):
-    training = asl.build_training(features)  # Experiment here with different feature sets defined in part 1
+    """Train on full training set."""
+    training = asl.build_training(features)
     sequences = training.get_all_sequences()
     Xlengths = training.get_all_Xlengths()
     model_dict = {}
     for word in training.words:
-        model = model_selector(sequences, Xlengths, word,
-                               n_constant=3).select()
+        model = model_selector(sequences, Xlengths, word, n_constant=3).select()
         model_dict[word]=model
     return model_dict
 
-
-# ### Recogniser implementation: experiments
-
-features_custom_names = ['custom_' + str(i) for i in range(1, 7)]
-features_other_names = ['ground', 'polar', 'delta', 'norm']
-features_all_names = features_other_names + features_custom_names
-features_for_experiments = ['features_' + name for name in all_names]
 
 model_selectors = ['SelectorCV', 'SelectorBIC', 'SelectorDIC']
 
 # Keep track of WER for all feature-selector combinations.
 wer_all = {}
 
+# Experiments: try feature-selector combinations
 start = timeit.default_timer()
 
-for features_name in features_for_experiments:
+for feature_set in all_features:
     print("===============")
-    print(features_name, end=" --- ")
-    features = eval(features_name) # get the actual object
+    print(feature_set, end=" --- ")
+    features = all_features[feature_set] # get the actual list of feature names
     print("Building test set...")
     test_set = asl.build_test(features)
     print("Number of test set items: {}".format(test_set.num_items))
@@ -195,7 +247,7 @@ for features_name in features_for_experiments:
         # Get the actual object
         model_selector = eval(model_selector_name)
         # Initialise dictionary for results
-        wer_all[features_name] = {}
+        wer_all[feature_set] = {}
         # Learn model on training data
         print("    Training word models...")
         models = train_all_words(features, model_selector)
@@ -203,14 +255,14 @@ for features_name in features_for_experiments:
         print("    Evaluating models...")
         probabilities, guesses = recognize(models, test_set)
         # Evaluate performance on test data
-        wer = show_errors(guesses, test_set)
+        wer = wer(guesses, test_set)
         print("    WER {}".format(wer))
         wer_all[features_name][model_selector_name] = wer
+        out_file = 'recognition_' + feature_set + '_' + model_selector_name
+        write_errors_to_file(guesses, test_set, out_file)
         print()
 
 end = timeit.default_timer()
 total_time = end - start
 print("total time: {}".format(total_time))
-
-# NOTE: I have modified the show_errors function so that now, in addition to displaying the recognised text alongside the correct one, it also returns a value, namely the WER. I needed this for my experiments, in order to compare my feature-selector combinations.
 
